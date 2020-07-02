3025e4b5af37353f10c296e816693329a27a4de4
AT <agnieszkatarsa@gmail.com>
2020-07-02 16:44:47 +0100

    Create Portal component

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-portal`,
+      options: {
+        key: "portal",
+        id: "portal",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
